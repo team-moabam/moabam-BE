eabe156120c94610988311cafad1a12342ca9bdd
Dev Uni <wlwhsvkdlxh@gmail.com>
2023-10-27 15:37:04 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/com/moabam/MoabamServerApplication.java
@@ -0,0 +1,13 @@
+package com.moabam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoabamServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoabamServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moabam/MoabamServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moabam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoabamServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
