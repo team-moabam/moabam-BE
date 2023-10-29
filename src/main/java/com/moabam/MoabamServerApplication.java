3c119f8509d0844f277358741ac3189bb9cdb4d0
parksey <parkseyeon99@naver.com>
2023-10-29 17:28:19 +0900

    feat: 카카오 OAuth 환경변수 추가 및 클래스 바인딩

--- src/main/java/com/moabam/MoabamServerApplication.java
+++ src/main/java/com/moabam/MoabamServerApplication.java
@@ -1,13 +1,19 @@
 package com.moabam;
 
+import com.moabam.global.config.OAuthConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class MoabamServerApplication {
+
+	@Autowired
+	OAuthConfig oAuthConfig;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoabamServerApplication.class, args);
 	}
-
 }
