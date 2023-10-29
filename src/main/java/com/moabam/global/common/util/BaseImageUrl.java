2cb395951b174c37490bdb09e68c8b8b99cf1c28
parksey <parkseyeon99@naver.com>
2023-10-29 13:42:37 +0900

    feat: 회원 엔티티 생성 및 테스트코드 추가

---
+++ src/main/java/com/moabam/global/common/util/BaseImageUrl.java
@@ -0,0 +1,14 @@
+package com.moabam.global.common.util;
+
+import lombok.Getter;
+
+@Getter
+public enum BaseImageUrl {
+	PROFILE_URL("/profile/baseUrl");
+
+	private String url;
+
+	BaseImageUrl(String url) {
+		this.url = url;
+	}
+}
---
+++ src/test/java/com/moabam/api/domain/member/MemberTest.java
@@ -0,0 +1,62 @@
+package com.moabam.api.domain.member;
+
+import com.moabam.global.common.util.BaseImageUrl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class MemberTest {
+
+	String socialId = "1";
+	String nickname = "밥세공기";
+	String profileImage = "kakao/profile/url";
+
+	@DisplayName("회원 생성 성공")
+	@Test
+	void create_member_success() {
+		// When + Then
+		assertThatNoException().isThrownBy(() -> Member.builder()
+			.socialId(socialId)
+			.nickname(nickname)
+			.profileImage(profileImage)
+			.build());
+	}
+
+	@DisplayName("프로필 이미지 없이 회원 생성 성공")
+	@Test
+	void create_member_noImage_success() {
+		// When + Then
+		assertThatNoException().isThrownBy(() -> {
+			Member member = Member.builder()
+				.socialId(socialId)
+				.nickname(nickname)
+				.profileImage(null)
+				.build();
+
+			assertAll(
+				() -> assertThat(member.getProfileImage()).isEqualTo(BaseImageUrl.PROFILE_URL.getUrl()),
+				() -> assertThat(member.getRole()).isEqualTo(Role.USER)
+			);
+		});
+	}
+
+	@DisplayName("소셜ID에 따른 회원 생성 실패")
+	@Test
+	void creat_member_failBy_socialId() {
+		// When + Then
+		assertThatThrownBy(Member.builder()
+			.nickname(nickname)::build)
+			.isInstanceOf(NullPointerException.class);
+	}
+
+	@DisplayName("닉네임에 따른 회원 생성 실패")
+	@Test
+	void create_member_failBy_nickname() {
+		// When + Then
+		assertThatThrownBy(Member.builder()
+			.socialId(socialId)::build)
+			.isInstanceOf(NullPointerException.class);
+	}
+}
