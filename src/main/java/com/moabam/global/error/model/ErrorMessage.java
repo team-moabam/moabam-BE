3e45ef93464929bcb5379c8ad07b33a73e23833b
kmebin <blowhuiee@gmail.com>
2023-10-30 22:10:07 +0900

    feat: 벌레 개수 검증 추가

--- src/main/java/com/moabam/global/error/model/ErrorMessage.java
+++ src/main/java/com/moabam/global/error/model/ErrorMessage.java
@@ -9,7 +9,9 @@ public enum ErrorMessage {
 
 	INVALID_REQUEST_FIELD("올바른 요청 정보가 아닙니다."),
 
-	MEMBER_NOT_FOUND("존재하지 않는 회원입니다.");
+	MEMBER_NOT_FOUND("존재하지 않는 회원입니다."),
+
+	INVALID_BUG_COUNT("벌레 개수는 0 이상이어야 합니다.");
 
 	private final String message;
 }
