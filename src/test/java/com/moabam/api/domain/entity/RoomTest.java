ece167849fc58dd4e40f8c186e6c6eefd6ae6743
ymkim97 <rladudaud97@naver.com>
2023-10-30 18:33:21 +0900

    test: Room 엔티티 테스트 코드 작성

---
+++ src/test/java/com/moabam/api/domain/entity/RoomTest.java
@@ -0,0 +1,88 @@
+package com.moabam.api.domain.entity;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import com.moabam.api.domain.entity.enums.RoomType;
+import com.moabam.global.error.exception.BadRequestException;
+import com.moabam.global.error.model.ErrorMessage;
+
+class RoomTest {
+
+	@DisplayName("비밀번호 없이 방 생성 성공")
+	@Test
+	void create_room_without_password_success() {
+		// given, when
+		Room room = Room.builder()
+			.title("앵윤이의 방")
+			.roomType(RoomType.MORNING)
+			.certifyTime(10)
+			.maxUserCount(9)
+			.announcement("HI Dev")
+			.build();
+
+		// then
+		assertThat(room.getPassword()).isNull();
+	}
+
+	@DisplayName("비밀번호 설정 후 방 생성 성공")
+	@Test
+	void create_room_with_password_success() {
+		// given, when
+		Room room = Room.builder()
+			.title("앵윤이의 방")
+			.password("12345")
+			.roomType(RoomType.MORNING)
+			.certifyTime(10)
+			.maxUserCount(9)
+			.announcement("HI Dev")
+			.build();
+
+		// then
+		assertThat(room.getPassword()).isEqualTo("12345");
+	}
+
+	@DisplayName("아침 방 설정 시, 저녁 시간이 들어오는 예외 발생")
+	@ParameterizedTest
+	@CsvSource({
+		"13", "19", "3", "11", "0"
+	})
+	void morning_time_validate_exception(int certifyTime) {
+		Room room = Room.builder()
+			.title("모아밤 짱")
+			.password("1234")
+			.roomType(RoomType.MORNING)
+			.certifyTime(9)
+			.maxUserCount(5)
+			.announcement("HELLO")
+			.build();
+
+		// given, when, then
+		assertThatThrownBy(() -> room.changeCertifyTime(certifyTime))
+			.isInstanceOf(BadRequestException.class)
+			.hasMessage(ErrorMessage.INVALID_REQUEST_FIELD.getMessage());
+	}
+
+	@DisplayName("저녁 방 설정 시, 아침 시간이 들어오는 경우 예외 발생")
+	@ParameterizedTest
+	@CsvSource({
+		"3", "5", "-1", "15", "8", "19"
+	})
+	void night_time_validate_exception(int certifyTime) {
+		Room room = Room.builder()
+			.title("모아밤 짱")
+			.roomType(RoomType.NIGHT)
+			.certifyTime(21)
+			.maxUserCount(5)
+			.build();
+
+		// given, when, then
+		assertThatThrownBy(() -> room.changeCertifyTime(certifyTime))
+			.isInstanceOf(BadRequestException.class)
+			.hasMessage(ErrorMessage.INVALID_REQUEST_FIELD.getMessage());
+	}
+}
