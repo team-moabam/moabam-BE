7b8ff993f70725f5f87db400c7056c161437f42d
ymkim97 <rladudaud97@naver.com>
2023-10-31 17:18:56 +0900

    test: 방 수정 통합 테스트 작성

--- src/test/java/com/moabam/api/presentation/RoomControllerTest.java
+++ src/test/java/com/moabam/api/presentation/RoomControllerTest.java
@@ -22,10 +22,13 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.moabam.api.domain.entity.Participant;
+import com.moabam.api.domain.entity.Room;
 import com.moabam.api.domain.repository.ParticipantRepository;
 import com.moabam.api.domain.repository.RoomRepository;
 import com.moabam.api.domain.repository.RoutineRepository;
 import com.moabam.api.dto.CreateRoomRequest;
+import com.moabam.api.dto.ModifyRoomRequest;
 
 @Transactional
 @SpringBootTest
@@ -193,7 +196,7 @@ class RoomControllerTest {
 			.andDo(print());
 	}
 
-	@DisplayName("올바르지 못한 시간으로 저녁 방 생성")
+	@DisplayName("올바르지 못한 시간으로 저녁 방 생성시 에외 발생")
 	@ParameterizedTest
 	@CsvSource({
 		"19", "3", "6", "9"
@@ -214,6 +217,71 @@ class RoomControllerTest {
 				.contentType(APPLICATION_JSON)
 				.content(json))
 			.andExpect(status().isBadRequest())
+			.andDo(print());
+	}
+
+	@DisplayName("방 수정 성공 - 방장일 경우")
+	@Test
+	void modify_room_success() throws Exception {
+		// given
+		Room room = Room.builder()
+			.title("처음 제목")
+			.password("1234")
+			.roomType(MORNING)
+			.certifyTime(9)
+			.maxUserCount(5)
+			.build();
+
+		Participant participant = Participant.builder()
+			.room(room)
+			.memberId(1L)
+			.build();
+		participant.enableManager();
+
+		Room savedRoom = roomRepository.save(room);
+		participantRepository.save(participant);
+
+		ModifyRoomRequest modifyRoomRequest = new ModifyRoomRequest("수정할 방임!", "1234", 10, 7);
+
+		String json = objectMapper.writeValueAsString(modifyRoomRequest);
+
+		// expected
+		mockMvc.perform(put("/rooms/" + savedRoom.getId())
+				.contentType(APPLICATION_JSON)
+				.content(json))
+			.andExpect(status().isOk())
+			.andDo(print());
+	}
+
+	@DisplayName("방 수정 실패 - 방장 아닐 경우")
+	@Test
+	void unauthorized_modify_room_fail() throws Exception {
+		// given
+		Room room = Room.builder()
+			.title("처음 제목")
+			.password("1234")
+			.roomType(MORNING)
+			.certifyTime(9)
+			.maxUserCount(5)
+			.build();
+
+		Participant participant = Participant.builder()
+			.room(room)
+			.memberId(1L)
+			.build();
+
+		Room savedRoom = roomRepository.save(room);
+		participantRepository.save(participant);
+
+		ModifyRoomRequest modifyRoomRequest = new ModifyRoomRequest("수정할 방임!", "1234", 10, 7);
+
+		String json = objectMapper.writeValueAsString(modifyRoomRequest);
+
+		// expected
+		mockMvc.perform(put("/rooms/" + savedRoom.getId())
+				.contentType(APPLICATION_JSON)
+				.content(json))
+			.andExpect(status().isNotFound())
 			.andDo(print());
 	}
 }
