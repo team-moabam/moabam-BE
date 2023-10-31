ea7170e96466f724209ae09dee4c72c9b7645633
kmebin <blowhuiee@gmail.com>
2023-10-31 15:44:18 +0900

    refactor: private 생성자 추가

--- src/main/java/com/moabam/api/dto/BugMapper.java
+++ src/main/java/com/moabam/api/dto/BugMapper.java
@@ -2,6 +2,10 @@ package com.moabam.api.dto;
 
 import com.moabam.api.domain.entity.Bug;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public final class BugMapper {
 
 	public static BugResponse from(Bug bug) {
