951d6080bf5f2d0fd397fa7cd8f3c3d16b802597
HyuckJuneHong <jjj8337721@naver.com>
2023-11-02 01:20:10 +0900

    refactor : 테스트 코드 Profile 변경

--- src/test/java/com/moabam/api/presentation/BugControllerTest.java
+++ src/test/java/com/moabam/api/presentation/BugControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -23,6 +24,7 @@ import com.moabam.fixture.BugFixture;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class BugControllerTest {
 
 	@Autowired
--- src/test/java/com/moabam/api/presentation/ProductControllerTest.java
+++ src/test/java/com/moabam/api/presentation/ProductControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -26,6 +27,7 @@ import com.moabam.fixture.ProductFixture;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class ProductControllerTest {
 
 	@Autowired
