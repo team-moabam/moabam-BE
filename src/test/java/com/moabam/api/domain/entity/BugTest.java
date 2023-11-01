48f49d252b083edbdab9c4715bf413d124f04a03
kmebin <blowhuiee@gmail.com>
2023-11-01 16:39:42 +0900

    fix: code smell 제거

--- src/test/java/com/moabam/api/domain/entity/BugTest.java
+++ src/test/java/com/moabam/api/domain/entity/BugTest.java
@@ -12,12 +12,13 @@ class BugTest {
 	@DisplayName("벌레 개수가 음수이면 예외가 발생한다.")
 	@Test
 	void negative_bug_count_throws_exception() {
-		assertThatThrownBy(() -> Bug.builder()
+		Bug.BugBuilder bugBuilder = Bug.builder()
 			.morningBug(10)
 			.nightBug(10)
-			.goldenBug(-10)
-			.build()
-		).isInstanceOf(BadRequestException.class)
+			.goldenBug(-10);
+
+		assertThatThrownBy(bugBuilder::build)
+			.isInstanceOf(BadRequestException.class)
 			.hasMessage("벌레 개수는 0 이상이어야 합니다.");
 	}
 }
