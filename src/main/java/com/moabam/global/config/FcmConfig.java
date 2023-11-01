aa0f8c4673bd0821e8fd0f4b551c81db9521b9ed
HyuckJuneHong <jjj8337721@naver.com>
2023-11-02 01:07:54 +0900

    refacotr: PostConstruct를 Bean으로 변경

--- src/main/java/com/moabam/global/config/FcmConfig.java
+++ src/main/java/com/moabam/global/config/FcmConfig.java
@@ -3,6 +3,7 @@ package com.moabam.global.config;
 import java.io.IOException;
 import java.io.InputStream;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,10 +11,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.moabam.global.error.exception.FcmException;
 import com.moabam.global.error.model.ErrorMessage;
 
-import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -23,8 +24,8 @@ public class FcmConfig {
 
 	private static final String FIREBASE_PATH = "config/moabam-firebase.json";
 
-	@PostConstruct
-	public void initFirebaseMessaging() {
+	@Bean
+	public FirebaseMessaging initFirebaseMessaging() {
 		try (InputStream inputStream = new ClassPathResource(FIREBASE_PATH).getInputStream()) {
 			GoogleCredentials credentials = GoogleCredentials.fromStream(inputStream);
 			FirebaseOptions firebaseOptions = FirebaseOptions.builder()
@@ -35,6 +36,8 @@ public class FcmConfig {
 				FirebaseApp.initializeApp(firebaseOptions);
 				log.info("======= Firebase init start =======");
 			}
+
+			return FirebaseMessaging.getInstance();
 		} catch (IOException e) {
 			throw new FcmException(ErrorMessage.FCM_INIT_FAILED);
 		}
