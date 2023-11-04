f0cf90d0e0f4eaef7d473fc19049cc09a9c1d110
HyuckJuneHong <jjj8337721@naver.com>
2023-11-04 19:28:12 +0900

    feat: Embedded Redis 환경 구축

--- src/main/java/com/moabam/global/common/util/GlobalConstant.java
+++ src/main/java/com/moabam/global/common/util/GlobalConstant.java
@@ -15,4 +15,14 @@ public class GlobalConstant {
 	public static final long EXPIRE_KNOCK = 12;
 	public static final long EXPIRE_FCM_TOKEN = 60;
 	public static final String FIREBASE_PATH = "config/moabam-firebase.json";
+
+	public static final String REDIS_SERVER_MAX_MEMORY = "maxmemory 128M";
+	public static final String REDIS_BINARY_PATH = "binary/redis/redis-server-arm64";
+	public static final String FIND_LISTEN_PROCESS_COMMAND = "netstat -nat | grep LISTEN | grep %d";
+	public static final String SHELL_PATH = "/bin/sh";
+	public static final String SHELL_COMMAND_OPTION = "-c";
+	public static final String OS_ARCHITECTURE = "os.arch";
+	public static final String OS_NAME = "os.name";
+	public static final String ARM_ARCHITECTURE = "aarch64";
+	public static final String MAC_OS_NAME = "Mac OS X";
 }
