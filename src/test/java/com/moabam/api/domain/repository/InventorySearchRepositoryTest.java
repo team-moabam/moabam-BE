f502897abfb5e57d9db924c6d9ec1a1888358998
kmebin <blowhuiee@gmail.com>
2023-11-04 17:32:53 +0900

    test: 인벤토리 아이템 목록 조회 Repository 테스트

---
+++ src/test/java/com/moabam/api/domain/repository/InventorySearchRepositoryTest.java
@@ -0,0 +1,67 @@
+package com.moabam.api.domain.repository;
+
+import static com.moabam.fixture.InventoryFixture.*;
+import static com.moabam.fixture.ItemFixture.*;
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.moabam.api.domain.entity.Item;
+import com.moabam.api.domain.entity.enums.RoomType;
+
+@SpringBootTest
+@Transactional(readOnly = true)
+class InventorySearchRepositoryTest {
+
+	@Autowired
+	ItemRepository itemRepository;
+
+	@Autowired
+	InventoryRepository inventoryRepository;
+
+	@Autowired
+	InventorySearchRepository inventorySearchRepository;
+
+	@DisplayName("타입으로 인벤토리에 있는 아이템 목록을 구매일 순으로 조회한다.")
+	@Test
+	void find_items_success() {
+		// given
+		Long memberId = 1L;
+		Item morningSantaSkin = itemRepository.save(morningSantaSkin());
+		inventoryRepository.save(inventory(memberId, morningSantaSkin));
+		Item morningKillerSkin = itemRepository.save(morningKillerSkin());
+		inventoryRepository.save(inventory(memberId, morningKillerSkin));
+		Item nightMageSkin = itemRepository.save(nightMageSkin());
+		inventoryRepository.save(inventory(memberId, nightMageSkin));
+
+		// when
+		List<Item> actual = inventorySearchRepository.findItems(memberId, RoomType.MORNING);
+
+		// then
+		assertThat(actual).hasSize(2)
+			.containsExactly(morningKillerSkin, morningSantaSkin);
+	}
+
+	@DisplayName("인벤토리에 해당하는 타입의 아이템이 없으면 빈 목록을 조회한다.")
+	@Test
+	void find_empty_success() {
+		// given
+		Long memberId = 1L;
+		Item morningSantaSkin = itemRepository.save(morningSantaSkin());
+		inventoryRepository.save(inventory(memberId, morningSantaSkin));
+		Item morningKillerSkin = itemRepository.save(morningKillerSkin());
+		inventoryRepository.save(inventory(memberId, morningKillerSkin));
+
+		// when
+		List<Item> actual = inventorySearchRepository.findItems(memberId, RoomType.NIGHT);
+
+		// then
+		assertThat(actual).isEmpty();
+	}
+}
---
+++ src/test/java/com/moabam/fixture/InventoryFixture.java
@@ -0,0 +1,14 @@
+package com.moabam.fixture;
+
+import com.moabam.api.domain.entity.Inventory;
+import com.moabam.api.domain.entity.Item;
+
+public class InventoryFixture {
+
+	public static Inventory inventory(Long memberId, Item item) {
+		return Inventory.builder()
+			.memberId(memberId)
+			.item(item)
+			.build();
+	}
+}
