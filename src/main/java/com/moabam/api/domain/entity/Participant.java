6435783a4ce811215bf6cbfe44d120cad0439b45
ymkim97 <rladudaud97@naver.com>
2023-11-06 03:30:18 +0900

    feat: Participant, Routine 수정, Search querydsl 작성

--- src/main/java/com/moabam/api/domain/entity/Participant.java
+++ src/main/java/com/moabam/api/domain/entity/Participant.java
@@ -6,6 +6,8 @@ import java.time.LocalDateTime;
 
 import org.hibernate.annotations.SQLDelete;
 
+import com.moabam.global.common.entity.BaseTimeEntity;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -25,7 +27,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "participant")
 @SQLDelete(sql = "UPDATE participant SET deleted_at = CURRENT_TIMESTAMP where id = ?")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Participant {
+public class Participant extends BaseTimeEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
