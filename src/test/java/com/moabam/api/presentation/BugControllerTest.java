344d99ef99e546e339bc33087adc7984ca66a3ba
kmebin <blowhuiee@gmail.com>
2023-11-05 16:56:05 +0900

    test: Controller 테스트 @WebMvcTest로 변경

--- src/test/java/com/moabam/api/presentation/BugControllerTest.java
+++ src/test/java/com/moabam/api/presentation/BugControllerTest.java
@@ -1,17 +1,18 @@
 package com.moabam.api.presentation;
 
+import static com.moabam.fixture.BugFixture.*;
 import static java.nio.charset.StandardCharsets.*;
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
+import static org.springframework.http.MediaType.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -19,10 +20,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.moabam.api.application.BugService;
 import com.moabam.api.dto.BugMapper;
 import com.moabam.api.dto.BugResponse;
-import com.moabam.fixture.BugFixture;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(BugController.class)
 class BugControllerTest {
 
 	@Autowired
@@ -39,17 +38,18 @@ class BugControllerTest {
 	void get_bug_success() throws Exception {
 		// given
 		Long memberId = 1L;
-		BugResponse expected = BugMapper.toBugResponse(BugFixture.bug());
+		BugResponse expected = BugMapper.toBugResponse(bug());
 		given(bugService.getBug(memberId)).willReturn(expected);
 
-		// expected
-		String content = mockMvc.perform(get("/bugs"))
+		// when, then
+		String content = mockMvc.perform(get("/bugs")
+				.contentType(APPLICATION_JSON))
 			.andDo(print())
 			.andExpect(status().isOk())
 			.andReturn()
 			.getResponse()
 			.getContentAsString(UTF_8);
 		BugResponse actual = objectMapper.readValue(content, BugResponse.class);
-		Assertions.assertThat(actual).isEqualTo(expected);
+		assertThat(actual).isEqualTo(expected);
 	}
 }
