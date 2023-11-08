a22f382ff1bc3e4c9398626951b0ae667476c6ba
HyuckJuneHong <jjj8337721@naver.com>
2023-11-08 23:41:03 +0900

    feat: 쿠폰 종류에 대한 조회 처리 구현 및 테스트

---
+++ src/test/java/com/moabam/api/domain/entity/enums/CouponTypeTest.java
@@ -0,0 +1,31 @@
+package com.moabam.api.domain.entity.enums;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.moabam.global.error.exception.NotFoundException;
+import com.moabam.global.error.model.ErrorMessage;
+
+class CouponTypeTest {
+
+	@DisplayName("존재하는 쿠폰을 가져오려고 할 때, - CouponType")
+	@Test
+	void couponType_from() {
+		// When
+		CouponType actual = CouponType.from("황금");
+
+		// Then
+		assertThat(actual).isEqualTo(CouponType.GOLDEN_COUPON);
+	}
+
+	@DisplayName("존재하지 않는 쿠폰을 가져오려고 할 때, - NotFoundException")
+	@Test
+	void couponType_from_NotFoundException() {
+		// When & Then
+		assertThatThrownBy(() -> CouponType.from("Not-Coupon"))
+			.isInstanceOf(NotFoundException.class)
+			.hasMessage(ErrorMessage.NOT_FOUND_COUPON_TYPE.getMessage());
+	}
+}
