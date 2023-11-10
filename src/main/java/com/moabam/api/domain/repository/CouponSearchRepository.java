d6f8adbcfafbf5156b6df7dfafa1898502c5af8e
HyuckJuneHong <jjj8337721@naver.com>
2023-11-10 20:07:45 +0900

    fix: 상태에 따른 잘못된 쿼리 수정

--- src/main/java/com/moabam/api/domain/repository/CouponSearchRepository.java
+++ src/main/java/com/moabam/api/domain/repository/CouponSearchRepository.java
@@ -41,22 +41,38 @@ public class CouponSearchRepository {
 			return null;
 		}
 
+		// 시작 전이거나 진행 중인 쿠폰들을 조회하고 싶은 경우
 		if (request.couponOngoing() && request.couponNotStarted()) {
-			return coupon.endAt.goe(now);
+			return (coupon.startAt.gt(now))
+				.or(coupon.startAt.loe(now).and(coupon.endAt.goe(now)));
 		}
 
+		// 종료 됐거나 진행 중인 쿠폰들을 조회하고 싶은 경우
 		if (request.couponOngoing() && request.couponEnded()) {
-			return coupon.startAt.loe(now);
+			return (coupon.endAt.lt(now))
+				.or(coupon.startAt.loe(now).and(coupon.endAt.goe(now)));
 		}
 
+		// 진행 중이 아니고, 시작 전이거나, 종료된 쿠폰들을 조회하고 싶은 경우
 		if (request.couponNotStarted() && request.couponEnded()) {
 			return coupon.startAt.gt(now)
 				.or(coupon.endAt.lt(now));
 		}
 
+		// 진행 중인 쿠폰들을 조회하고 싶은 경우
 		if (request.couponOngoing()) {
 			return coupon.startAt.loe(now)
 				.and(coupon.endAt.goe(now));
+		}
+
+		// 시작 적인 쿠폰들을 조회하고 싶은 경우
+		if (request.couponNotStarted()) {
+			return coupon.startAt.gt(now);
+		}
+
+		// 종료된 쿠폰들을 조회하고 싶은 경우
+		if (request.couponEnded()) {
+			return coupon.endAt.lt(now);
 		}
 
 		return Expressions.FALSE;
