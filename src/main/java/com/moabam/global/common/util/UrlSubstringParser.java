a347b6ad6f4f5f01134c562e16787bbfb6c99637
Dev Uni <wlwhsvkdlxh@gmail.com>
2023-11-10 21:13:18 +0900

    feat: UrlSubstringParser 구현

---
+++ src/main/java/com/moabam/global/common/util/UrlSubstringParser.java
@@ -0,0 +1,21 @@
+package com.moabam.global.common.util;
+
+import static com.moabam.global.common.util.GlobalConstant.*;
+
+import com.moabam.global.error.exception.BadRequestException;
+import com.moabam.global.error.model.ErrorMessage;
+
+public class UrlSubstringParser {
+
+	public static String parseUrl(String url, String distinctToken) {
+
+		int lastSlashTokenIndex = url.lastIndexOf(DELIMITER);
+		int distinctTokenIndex = url.indexOf(distinctToken);
+
+		if (lastSlashTokenIndex == -1 || distinctTokenIndex == -1 || lastSlashTokenIndex > distinctTokenIndex) {
+			throw new BadRequestException(ErrorMessage.INVALID_REQUEST_URL);
+		}
+
+		return url.substring(lastSlashTokenIndex + 1, distinctTokenIndex);
+	}
+}
