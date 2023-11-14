1994c552f413dcd9e99a427c03fbeef925c27b00
ymkim97 <rladudaud97@naver.com>
2023-11-15 01:43:42 +0900

    refactor: 테스트 패키지 구조 변경

--- src/test/java/com/moabam/api/domain/room/CertificationTest.java
+++ src/test/java/com/moabam/api/domain/room/CertificationTest.java
@@ -1,14 +1,9 @@
-package com.moabam.api.domain.entity;
+package com.moabam.api.domain.room;
 
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import com.moabam.api.domain.room.Certification;
-import com.moabam.api.domain.room.Room;
-import com.moabam.api.domain.room.RoomType;
-import com.moabam.api.domain.room.Routine;
 
 class CertificationTest {
 
