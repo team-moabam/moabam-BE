0143d428f7ccee8da8d8b5d35bf37e4c71e67a5e
kmebin <blowhuiee@gmail.com>
2023-11-14 17:54:16 +0900

    refactor: 상품 컨트롤러 통합 테스트로 변경

--- src/test/java/com/moabam/api/presentation/ProductControllerTest.java
+++ src/test/java/com/moabam/api/presentation/ProductControllerTest.java
@@ -3,7 +3,6 @@ package com.moabam.api.presentation;
 import static com.moabam.support.fixture.ProductFixture.*;
 import static java.nio.charset.StandardCharsets.*;
 import static org.assertj.core.api.Assertions.*;
-import static org.mockito.BDDMockito.*;
 import static org.springframework.http.MediaType.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
@@ -14,18 +13,21 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.moabam.api.application.product.ProductMapper;
-import com.moabam.api.application.product.ProductService;
 import com.moabam.api.domain.product.Product;
+import com.moabam.api.domain.product.repository.ProductRepository;
 import com.moabam.api.dto.product.ProductsResponse;
 import com.moabam.support.common.WithoutFilterSupporter;
 
-@WebMvcTest(controllers = ProductController.class)
+@Transactional
+@SpringBootTest
+@AutoConfigureMockMvc
 class ProductControllerTest extends WithoutFilterSupporter {
 
 	@Autowired
@@ -34,23 +36,21 @@ class ProductControllerTest extends WithoutFilterSupporter {
 	@Autowired
 	ObjectMapper objectMapper;
 
-	@MockBean
-	ProductService productService;
+	@Autowired
+	ProductRepository productRepository;
 
 	@DisplayName("상품 목록을 조회한다.")
 	@Test
 	void get_products_success() throws Exception {
 		// given
-		Product product1 = bugProduct();
-		Product product2 = bugProduct();
-		ProductsResponse expected = ProductMapper.toProductsResponse(List.of(product1, product2));
-		given(productService.getProducts()).willReturn(expected);
+		List<Product> products = productRepository.saveAll(List.of(bugProduct(), bugProduct()));
+		ProductsResponse expected = ProductMapper.toProductsResponse(products);
 
 		// when, then
 		String content = mockMvc.perform(get("/products")
 				.contentType(APPLICATION_JSON))
-			.andDo(print())
 			.andExpect(status().isOk())
+			.andDo(print())
 			.andReturn()
 			.getResponse()
 			.getContentAsString(UTF_8);
