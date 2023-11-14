5e82457bb64d3abf761df23f8921538e3ae23e74
kmebin <blowhuiee@gmail.com>
2023-11-14 14:31:43 +0900

    test: 벌레 조회 Controller 통합 테스트

--- src/test/java/com/moabam/api/presentation/BugControllerTest.java
+++ src/test/java/com/moabam/api/presentation/BugControllerTest.java
@@ -1,28 +1,28 @@
 package com.moabam.api.presentation;
 
-import static com.moabam.support.fixture.BugFixture.*;
-import static java.nio.charset.StandardCharsets.*;
-import static org.assertj.core.api.Assertions.*;
-import static org.mockito.BDDMockito.*;
+import static com.moabam.support.fixture.MemberFixture.*;
 import static org.springframework.http.MediaType.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.moabam.api.application.bug.BugMapper;
-import com.moabam.api.application.bug.BugService;
-import com.moabam.api.dto.bug.BugResponse;
+import com.moabam.api.domain.member.repository.MemberRepository;
+import com.moabam.support.annotation.WithMember;
 import com.moabam.support.common.WithoutFilterSupporter;
 
-@WebMvcTest(controllers = BugController.class)
+@Transactional
+@SpringBootTest
+@AutoConfigureMockMvc
 class BugControllerTest extends WithoutFilterSupporter {
 
 	@Autowired
@@ -31,26 +31,35 @@ class BugControllerTest extends WithoutFilterSupporter {
 	@Autowired
 	ObjectMapper objectMapper;
 
-	@MockBean
-	BugService bugService;
+	@Autowired
+	MemberRepository memberRepository;
 
 	@DisplayName("벌레를 조회한다.")
-	@Test
-	void get_bug_success() throws Exception {
-		// given
-		Long memberId = 1L;
-		BugResponse expected = BugMapper.toBugResponse(bug());
-		given(bugService.getBug(memberId)).willReturn(expected);
+	@Nested
+	class GetBug {
 
-		// when, then
-		String content = mockMvc.perform(get("/bugs")
-				.contentType(APPLICATION_JSON))
-			.andDo(print())
-			.andExpect(status().isOk())
-			.andReturn()
-			.getResponse()
-			.getContentAsString(UTF_8);
-		BugResponse actual = objectMapper.readValue(content, BugResponse.class);
-		assertThat(actual).isEqualTo(expected);
+		@DisplayName("성공한다.")
+		@WithMember
+		@Test
+		void success() throws Exception {
+			// given
+			memberRepository.save(member());
+
+			// expected
+			mockMvc.perform(get("/bugs")
+					.contentType(APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andDo(print());
+		}
+
+		@DisplayName("해당 회원이 존재하지 않으면 예외가 발생한다.")
+		@WithMember
+		@Test
+		void member_not_found_exception() throws Exception {
+			mockMvc.perform(get("/bugs")
+					.contentType(APPLICATION_JSON))
+				.andExpect(status().isNotFound())
+				.andDo(print());
+		}
 	}
 }
