06909262d784fdaaa0498db6324757bffbd4ad71
kmebin <blowhuiee@gmail.com>
2023-11-14 15:02:08 +0900

    chore: 주석 제거

--- src/main/java/com/moabam/api/domain/repository/BugHistorySearchRepository.java
+++ src/main/java/com/moabam/api/domain/repository/BugHistorySearchRepository.java
@@ -1,5 +1,7 @@
 package com.moabam.api.domain.repository;
 
+import static com.moabam.api.domain.bug.QBugHistory.*;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Repository;
 
 import com.moabam.api.domain.bug.BugActionType;
 import com.moabam.api.domain.bug.BugHistory;
+import com.moabam.global.common.util.DynamicQuery;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
@@ -19,21 +22,19 @@ public class BugHistorySearchRepository {
 	private final JPAQueryFactory jpaQueryFactory;
 
 	public List<BugHistory> find(Long memberId, BugActionType actionType, LocalDateTime dateTime) {
-		// return jpaQueryFactory
-		// 	.selectFrom(bugHistory)
-		// 	.where(
-		// 		DynamicQuery.generateEq(memberId, bugHistory.memberId::eq),
-		// 		DynamicQuery.generateEq(actionType, bugHistory.actionType::eq),
-		// 		DynamicQuery.generateEq(dateTime, this::equalDate)
-		// 	)
-		// 	.fetch();
-		return null;
+		return jpaQueryFactory
+			.selectFrom(bugHistory)
+			.where(
+				DynamicQuery.generateEq(memberId, bugHistory.memberId::eq),
+				DynamicQuery.generateEq(actionType, bugHistory.actionType::eq),
+				DynamicQuery.generateEq(dateTime, this::equalDate)
+			)
+			.fetch();
 	}
 
 	private BooleanExpression equalDate(LocalDateTime dateTime) {
-		return null;
-		// return bugHistory.createdAt.year().eq(dateTime.getYear())
-		// 	.and(bugHistory.createdAt.month().eq(dateTime.getMonthValue()))
-		// 	.and(bugHistory.createdAt.dayOfMonth().eq(dateTime.getDayOfMonth()));
+		return bugHistory.createdAt.year().eq(dateTime.getYear())
+			.and(bugHistory.createdAt.month().eq(dateTime.getMonthValue()))
+			.and(bugHistory.createdAt.dayOfMonth().eq(dateTime.getDayOfMonth()));
 	}
 }
