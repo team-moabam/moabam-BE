7ffda979ea205cdc330cc98ca38d32096712830f
Dev Uni <wlwhsvkdlxh@gmail.com>
2023-11-14 16:58:22 +0900

    feat: 쉘 스크립트 파일 추가

--- scripts/init-nginx-converter.sh
+++ scripts/init-nginx-converter.sh
@@ -7,7 +7,8 @@ fi
 
 export SERVER_DOMAIN=${SERVER_DOMAIN}
 export SERVER_PORT=${SERVER_PORT}
-export RESOLVER_IP=${RESOLVER_IP}
 export BLUE_CONTAINER=${BLUE_CONTAINER}
 
-envsubst '$SERVER_DOMAIN $SERVER_PORT $RESOLVER_IP $BLUE_CONTAINER' < /home/ubuntu/moabam/nginx/nginx.template > /home/ubuntu/moabam/nginx/nginx.conf
+envsubst '$SERVER_DOMAIN' < /home/ubuntu/moabam/nginx/templates/http-server.template > /home/ubuntu/moabam/nginx/conf.d/http-server.conf
+envsubst '$SERVER_DOMAIN' < /home/ubuntu/moabam/nginx/templates/ssl-server.template > /home/ubuntu/moabam/nginx/conf.d/ssl-server.conf
+envsubst '$BLUE_CONTAINER $SERVER_PORT' < /home/ubuntu/moabam/nginx/templates/upstream.template > /home/ubuntu/moabam/nginx/conf.d/upstream.conf
