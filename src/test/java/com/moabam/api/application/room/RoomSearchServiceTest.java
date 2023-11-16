60b4f3625cfb43d1cbf6c049077fe43d6d8d431a
Dev Uni <wlwhsvkdlxh@gmail.com>
2023-11-16 17:26:41 +0900

    test: 방 참여 기록 조회 서비스 테스트

--- src/test/java/com/moabam/api/application/room/RoomSearchServiceTest.java
+++ src/test/java/com/moabam/api/application/room/RoomSearchServiceTest.java
@@ -1,9 +1,12 @@
 package com.moabam.api.application.room;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.mockito.BDDMockito.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.spy;
+import static org.mockito.Mockito.when;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
@@ -19,8 +22,10 @@ import com.moabam.api.domain.room.Room;
 import com.moabam.api.domain.room.RoomType;
 import com.moabam.api.domain.room.repository.CertificationsSearchRepository;
 import com.moabam.api.domain.room.repository.ParticipantSearchRepository;
+import com.moabam.api.domain.room.repository.RoomRepository;
 import com.moabam.api.domain.room.repository.RoutineSearchRepository;
 import com.moabam.api.dto.room.MyRoomsResponse;
+import com.moabam.api.dto.room.RoomsHistoryResponse;
 import com.moabam.support.fixture.RoomFixture;
 
 @ExtendWith(MockitoExtension.class)
@@ -44,6 +49,9 @@ class RoomSearchServiceTest {
 	@Mock
 	private RoomCertificationService certificationService;
 
+	@Mock
+	private RoomRepository roomRepository;
+
 	@DisplayName("유저가 참여중인 방 목록 조회 성공")
 	@Test
 	void get_my_rooms_success() {
@@ -54,16 +62,16 @@ class RoomSearchServiceTest {
 		Room room2 = spy(RoomFixture.room("아침 - 두 번째 방", RoomType.MORNING, 9));
 		Room room3 = spy(RoomFixture.room("밤 - 첫 번째 방", RoomType.NIGHT, 22));
 
-		lenient().when(room1.getId()).thenReturn(1L);
-		lenient().when(room2.getId()).thenReturn(2L);
-		lenient().when(room3.getId()).thenReturn(3L);
+		when(room1.getId()).thenReturn(1L);
+		when(room2.getId()).thenReturn(2L);
+		when(room3.getId()).thenReturn(3L);
 
 		Participant participant1 = RoomFixture.participant(room1, memberId);
 		Participant participant2 = RoomFixture.participant(room2, memberId);
 		Participant participant3 = RoomFixture.participant(room3, memberId);
 		List<Participant> participants = List.of(participant1, participant2, participant3);
 
-		given(participantSearchRepository.findParticipantsByMemberId(memberId)).willReturn(participants);
+		given(participantSearchRepository.findNotDeletedParticipantsByMemberId(memberId)).willReturn(participants);
 		given(certificationService.existsMemberCertification(memberId, room1.getId(), today)).willReturn(true);
 		given(certificationService.existsMemberCertification(memberId, room2.getId(), today)).willReturn(false);
 		given(certificationService.existsMemberCertification(memberId, room3.getId(), today)).willReturn(true);
@@ -86,5 +94,44 @@ class RoomSearchServiceTest {
 
 		assertThat(myRooms.participatingRooms().get(2).isMemberCertifiedToday()).isTrue();
 		assertThat(myRooms.participatingRooms().get(2).isRoomCertifiedToday()).isFalse();
+	}
+
+	@DisplayName("방 참여 기록 조회 성공")
+	@Test
+	void get_my_join_history_success() {
+		// given
+		LocalDateTime today = LocalDateTime.now();
+		Long memberId = 1L;
+		Room room1 = spy(RoomFixture.room("아침 - 첫 번째 방", RoomType.MORNING, 10));
+		Room room2 = spy(RoomFixture.room("아침 - 두 번째 방", RoomType.MORNING, 9));
+		Room room3 = RoomFixture.room("밤 - 첫 번째 방", RoomType.NIGHT, 22);
+
+		when(room1.getId()).thenReturn(1L);
+		when(room2.getId()).thenReturn(2L);
+
+		Participant participant1 = RoomFixture.participant(room1, memberId);
+		Participant participant2 = RoomFixture.participant(room2, memberId);
+		Participant participant3 = spy(RoomFixture.participant(room3, memberId));
+		participant3.removeRoom();
+		List<Participant> participants = List.of(participant1, participant2, participant3);
+
+		when(participant3.getDeletedAt()).thenReturn(today);
+		when(participant3.getDeletedRoomTitle()).thenReturn("밤 - 첫 번째 방");
+		given(participantSearchRepository.findAllParticipantsByMemberId(memberId)).willReturn(participants);
+
+		// when
+		RoomsHistoryResponse response = roomSearchService.getJoinHistory(memberId);
+
+		// then
+		assertThat(response.roomHistory()).hasSize(3);
+
+		assertThat(response.roomHistory().get(0).deletedAt()).isNull();
+		assertThat(response.roomHistory().get(0).title()).isEqualTo(room1.getTitle());
+
+		assertThat(response.roomHistory().get(1).deletedAt()).isNull();
+		assertThat(response.roomHistory().get(1).title()).isEqualTo(room2.getTitle());
+
+		assertThat(response.roomHistory().get(2).deletedAt()).isNotNull();
+		assertThat(response.roomHistory().get(2).title()).isEqualTo(participant3.getDeletedRoomTitle());
 	}
 }
