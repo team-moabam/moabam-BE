adfb5cd06c6050eadb6b1aa2747701588cbbdaf0
parksey <parkseyeon99@naver.com>
2023-11-16 20:40:19 +0900

    feat: profile 환경에 따른 cookie 설정 분리 및 config 업데이트

---
+++ src/main/java/com/moabam/global/common/util/cookie/CookieProdUtils.java
@@ -0,0 +1,22 @@
+package com.moabam.global.common.util.cookie;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.http.Cookie;
+
+@Component
+@Profile({"prod"})
+public class CookieProdUtils extends CookieUtils {
+
+	protected Cookie detailCookies(String name, String value, long expireTime) {
+		Cookie cookie = new Cookie(name, value);
+		cookie.setSecure(true);
+		cookie.setHttpOnly(true);
+		cookie.setPath("/");
+		cookie.setMaxAge((int)expireTime);
+
+		return cookie;
+	}
+}
+
