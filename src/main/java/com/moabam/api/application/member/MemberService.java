9acb1e3b782cba87c33450384c084be22daf425f
ymkim97 <rladudaud97@naver.com>
2023-11-17 20:15:11 +0900

    refactor: 방장 member 반환 기능 삭제

--- src/main/java/com/moabam/api/application/member/MemberService.java
+++ src/main/java/com/moabam/api/application/member/MemberService.java
@@ -13,7 +13,6 @@ import org.springframework.transaction.annotation.Transactional;
 import com.moabam.api.application.auth.mapper.AuthMapper;
 import com.moabam.api.domain.member.Member;
 import com.moabam.api.domain.member.repository.MemberRepository;
-import com.moabam.api.domain.member.repository.MemberSearchRepository;
 import com.moabam.api.dto.auth.AuthorizationTokenInfoResponse;
 import com.moabam.api.dto.auth.LoginResponse;
 import com.moabam.global.error.exception.NotFoundException;
@@ -26,7 +25,6 @@ import lombok.RequiredArgsConstructor;
 public class MemberService {
 
 	private final MemberRepository memberRepository;
-	private final MemberSearchRepository memberSearchRepository;
 
 	public Member getById(Long memberId) {
 		return memberRepository.findById(memberId)
@@ -51,11 +49,6 @@ public class MemberService {
 	private String createRandomNickName() {
 		return RandomStringUtils.random(6, 0, 0, true, true, null,
 			new SecureRandom());
-	}
-
-	public Member getManager(Long roomId) {
-		return memberSearchRepository.findManager(roomId)
-			.orElseThrow(() -> new NotFoundException(MEMBER_NOT_FOUND));
 	}
 
 	public List<Member> getRoomMembers(List<Long> memberIds) {
