938bae3919aad82d1c088ef03297474b1f497701
kmebin <blowhuiee@gmail.com>
2023-11-18 17:09:26 +0900

    style: 메서드 네이밍 수정

--- src/main/java/com/moabam/api/application/bug/BugService.java
+++ src/main/java/com/moabam/api/application/bug/BugService.java
@@ -68,7 +68,7 @@ public class BugService {
 
 	@Transactional
 	public PurchaseProductResponse purchaseBugProduct(Long memberId, Long productId, PurchaseProductRequest request) {
-		Product product = getById(productId);
+		Product product = getProduct(productId);
 		Payment payment = PaymentMapper.toEntity(memberId, product);
 
 		if (!isNull(request.couponId())) {
@@ -89,7 +89,7 @@ public class BugService {
 			.sum();
 	}
 
-	private Product getById(Long productId) {
+	private Product getProduct(Long productId) {
 		return productRepository.findById(productId)
 			.orElseThrow(() -> new NotFoundException(PRODUCT_NOT_FOUND));
 	}
