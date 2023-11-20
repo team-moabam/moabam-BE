6a22c6942c37e180ca6b6cd5ee7dcc144d83413f
parksey <parkseyeon99@naver.com>
2023-11-20 16:44:01 +0900

    feat: Cookie secure 추가

--- src/main/java/com/moabam/global/common/util/CookieUtils.java
+++ src/main/java/com/moabam/global/common/util/CookieUtils.java
@@ -1,34 +1,37 @@
 package com.moabam.global.common.util;
 
 import jakarta.servlet.http.Cookie;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CookieUtils {
 
 	public static Cookie tokenCookie(String name, String value, long expireTime) {
 		Cookie cookie = new Cookie(name, value);
-
-		return detailCookies(cookie, expireTime);
-	}
-
-	public static Cookie typeCookie(String value, long expireTime) {
-		Cookie cookie = new Cookie("token_type", value);
-
-		return detailCookies(cookie, expireTime);
-	}
-
-	public static Cookie deleteCookie(Cookie cookie) {
-		cookie.setMaxAge(0);
-		cookie.setPath("/");
-		return cookie;
-	}
-
-	private static Cookie detailCookies(Cookie cookie, long expireTime) {
 		cookie.setSecure(true);
 		cookie.setHttpOnly(true);
 		cookie.setPath("/");
 		cookie.setMaxAge((int)expireTime);
 		cookie.setAttribute("SameSite", "Lax");
 
+		return cookie;
+	}
+
+	public static Cookie typeCookie(String value, long expireTime) {
+		Cookie cookie = new Cookie("token_type", value);
+		cookie.setSecure(true);
+		cookie.setHttpOnly(true);
+		cookie.setPath("/");
+		cookie.setMaxAge((int)expireTime);
+		cookie.setAttribute("SameSite", "Lax");
+
+		return cookie;
+	}
+
+	public static Cookie deleteCookie(Cookie cookie) {
+		cookie.setMaxAge(0);
+		cookie.setPath("/");
 		return cookie;
 	}
 }
