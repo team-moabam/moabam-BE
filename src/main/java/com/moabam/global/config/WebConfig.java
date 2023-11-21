136eff2a1546ad31a8f211756aebcb82019de8ae
HyuckJuneHong <jjj8337721@naver.com>
2023-11-21 18:19:13 +0900

    style : Schedule 어노테이션 위치 변경

--- src/main/java/com/moabam/global/config/WebConfig.java
+++ src/main/java/com/moabam/global/config/WebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -15,6 +16,7 @@ import com.moabam.global.auth.handler.AuthArgumentResolver;
 import com.moabam.global.auth.handler.PathResolver;
 
 @Configuration
+@EnableScheduling
 public class WebConfig implements WebMvcConfigurer {
 
 	private static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
