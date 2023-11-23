90780d944e0303f3b366dce307ca66db002f8b74
HyuckJuneHong <jjj8337721@naver.com>
2023-11-24 01:24:54 +0900

    feat: 쿠폰 보관함 저장소 조회 기능 구현 및 테스트

--- src/test/java/com/moabam/api/domain/coupon/repository/CouponWalletSearchRepositoryTest.java
+++ src/test/java/com/moabam/api/domain/coupon/repository/CouponWalletSearchRepositoryTest.java
@@ -3,8 +3,12 @@ package com.moabam.api.domain.coupon.repository;
 import static com.moabam.support.fixture.CouponFixture.*;
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.moabam.api.domain.coupon.Coupon;
@@ -22,6 +26,57 @@ class CouponWalletSearchRepositoryTest {
 
 	@Autowired
 	private CouponWalletSearchRepository couponWalletSearchRepository;
+
+	@DisplayName("ID가 1인 회원은 쿠폰 1개를 가지고 있다. - List<CouponWallet>")
+	@MethodSource("com.moabam.support.fixture.CouponWalletFixture#provideCouponWalletAll")
+	@ParameterizedTest
+	void findAllByCouponIdAndMemberId_Id1_success(List<CouponWallet> couponWallets) {
+		// Given
+		couponWallets.forEach(couponWallet -> {
+			Coupon coupon = couponRepository.save(couponWallet.getCoupon());
+			couponWalletRepository.save(CouponWallet.create(couponWallet.getMemberId(), coupon));
+		});
+
+		// When
+		List<CouponWallet> actual = couponWalletSearchRepository.findAllByCouponIdAndMemberId(null, 1L);
+
+		// Then
+		assertThat(actual).hasSize(1);
+	}
+
+	@DisplayName("ID가 2인 회원은 쿠폰 ID가 777인 쿠폰을 가지고 있지 않다. - List<CouponWallet>")
+	@MethodSource("com.moabam.support.fixture.CouponWalletFixture#provideCouponWalletAll")
+	@ParameterizedTest
+	void findAllByCouponIdAndMemberId_Id2_notCouponId777(List<CouponWallet> couponWallets) {
+		// Given
+		couponWallets.forEach(couponWallet -> {
+			Coupon coupon = couponRepository.save(couponWallet.getCoupon());
+			couponWalletRepository.save(CouponWallet.create(couponWallet.getMemberId(), coupon));
+		});
+
+		// When
+		List<CouponWallet> actual = couponWalletSearchRepository.findAllByCouponIdAndMemberId(777L, 2L);
+
+		// Then
+		assertThat(actual).isEmpty();
+	}
+
+	@DisplayName("ID가 3인 회원은 쿠폰 3개를 가지고 있다. - List<CouponWallet>")
+	@MethodSource("com.moabam.support.fixture.CouponWalletFixture#provideCouponWalletAll")
+	@ParameterizedTest
+	void findAllByCouponIdAndMemberId_Id3_success(List<CouponWallet> couponWallets) {
+		// Given
+		couponWallets.forEach(couponWallet -> {
+			Coupon coupon = couponRepository.save(couponWallet.getCoupon());
+			couponWalletRepository.save(CouponWallet.create(couponWallet.getMemberId(), coupon));
+		});
+
+		// When
+		List<CouponWallet> actual = couponWalletSearchRepository.findAllByCouponIdAndMemberId(null, 3L);
+
+		// Then
+		assertThat(actual).hasSize(3);
+	}
 
 	@DisplayName("회원의 특정 쿠폰 지갑을 성공적으로 조회한다.")
 	@Test
