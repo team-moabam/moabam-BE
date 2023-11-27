8128c6b068e82f40798c500e70dabe3868ecca64
ymkim97 <rladudaud97@naver.com>
2023-11-27 15:11:11 +0900

    feat: Image Type 추가

--- src/main/java/com/moabam/api/domain/image/ImageName.java
+++ src/main/java/com/moabam/api/domain/image/ImageName.java
@@ -17,15 +17,19 @@ public class ImageName {
 
 	private static final String CERTIFICATION_PATH = "certifications" + DELIMITER + LocalDate.now() + DELIMITER;
 	private static final String PROFILE_IMAGE = "members/profile" + DELIMITER;
+	private static final String BIRD_SKIN = "moabam/skins" + DELIMITER;
 	private static final String DEFAULT = "moabam/default" + DELIMITER;
 
 	private final String fileName;
 
 	public static ImageName of(MultipartFile file, ImageType imageType) {
 		return switch (imageType) {
-			case CERTIFICATION -> new ImageName(CERTIFICATION_PATH + file.getName() + "_" + UUID.randomUUID());
-			case PROFILE_IMAGE -> new ImageName(PROFILE_IMAGE + file.getName() + "_" + UUID.randomUUID());
-			case DEFAULT -> new ImageName(DEFAULT + file.getName());
+			case CERTIFICATION ->
+				new ImageName(CERTIFICATION_PATH + file.getName() + "_" + UUID.randomUUID() + IMAGE_EXTENSION);
+			case PROFILE_IMAGE ->
+				new ImageName(PROFILE_IMAGE + file.getName() + "_" + UUID.randomUUID() + IMAGE_EXTENSION);
+			case BIRD_SKIN -> new ImageName(BIRD_SKIN + file.getName() + IMAGE_EXTENSION);
+			case DEFAULT -> new ImageName(DEFAULT + file.getName() + IMAGE_EXTENSION);
 		};
 	}
 }
