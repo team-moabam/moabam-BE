b9d23a62c018110ef94179ff812f373d2f50b844
Dev Uni <wlwhsvkdlxh@gmail.com>
2023-11-28 18:51:43 +0900

    hotfix: config admin key 업데이트

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,9 +1,9 @@
 insert into item (type, category, name, awake_image, sleep_image, unlock_level, created_at)
-values ('MORNING', 'SKIN', '오목눈이알', 'https://image.moabam.com/moabam/skins/omok/default/egg.png',
+values ('MORNING', 'SKIN', '오목눈이 알', 'https://image.moabam.com/moabam/skins/omok/default/egg.png',
         'https://image.moabam.com/moabam/skins/omok/default/egg.png', 0, current_time());
 
 insert into item (type, category, name, awake_image, sleep_image, unlock_level, created_at)
-values ('NIGHT', 'SKIN', '부엉이알', 'https://image.moabam.com/moabam/skins/owl/default/egg.png',
+values ('NIGHT', 'SKIN', '부엉이 알', 'https://image.moabam.com/moabam/skins/owl/default/egg.png',
         'https://image.moabam.com/moabam/skins/owl/default/egg.png', 0, current_time());
 
 insert into item (type, category, name, awake_image, sleep_image, unlock_level, created_at)
@@ -31,8 +31,8 @@ values ('NIGHT', 'SKIN', '목도리 부엉이', 'https://image.moabam.com/moabam
         'https://image.moabam.com/moabam/skins/owl/scarf/eyes-closed', 20, 10, 10, current_time());
 
 insert into item (type, category, name, awake_image, sleep_image, bug_price, golden_bug_price, unlock_level, created_at)
-values ('MORNING', 'SKIN', '산타 오목눈이', 'https://image.moabam.com/moabam/skins/omok/scarf/eyes-opened.png',
-        'https://image.moabam.com/moabam/skins/omok/scarf/eyes-closed', 30, 15, 15, current_time());
+values ('MORNING', 'SKIN', '산타 오목눈이', 'https://image.moabam.com/moabam/skins/omok/santa/eyes-opened.png',
+        'https://image.moabam.com/moabam/skins/omok/santa/eyes-closed', 30, 15, 15, current_time());
 
 insert into item (type, category, name, awake_image, sleep_image, bug_price, golden_bug_price, unlock_level, created_at)
 values ('NIGHT', 'SKIN', '산타 부엉이', 'https://image.moabam.com/moabam/skins/owl/santa/eyes-opened.png',
