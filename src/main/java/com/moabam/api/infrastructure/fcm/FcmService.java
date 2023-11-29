c3a8f78e163d28e2fdf5dafc58faa421cea6ef8c
parksey <parkseyeon99@naver.com>
2023-11-29 23:30:16 +0900

    style: 필요없는 로그 제거

--- src/main/java/com/moabam/api/infrastructure/fcm/FcmService.java
+++ src/main/java/com/moabam/api/infrastructure/fcm/FcmService.java
@@ -9,9 +9,7 @@ import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.Notification;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class FcmService {
@@ -24,9 +22,7 @@ public class FcmService {
 			return;
 		}
 
-		log.info("FCM TOKEN before: " + fcmToken);
 		fcmRepository.saveToken(fcmToken, memberId);
-		log.info("FCM TOKEN after: " + findTokenByMemberId(memberId));
 	}
 
 	public void deleteTokenByMemberId(Long memberId) {
