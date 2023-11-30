286820bfed2b05fe75923ec67a309bb661fb1186
Park Seyeon <parkseyeon99@naver.com>
2023-11-30 16:13:56 +0900

    refactor: 랭킹 응답 반환 객체 변수면 s 제거
    Co-authored-by: Kim Heebin <blowhuiee@gmail.com>

--- src/main/java/com/moabam/api/dto/ranking/TopRankingResponses.java
+++ src/main/java/com/moabam/api/dto/ranking/TopRankingResponses.java
@@ -5,7 +5,7 @@ import java.util.List;
 import lombok.Builder;
 
 @Builder
-public record TopRankingResponses(
+public record TopRankingResponse(
 	List<TopRankingInfo> topRankings,
 	TopRankingInfo myRanking
 ) {
