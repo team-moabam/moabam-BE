7add117474a557f2d0dfcf4d6b2b15b28fe3065f
kmebin <blowhuiee@gmail.com>
2023-11-30 14:54:41 +0900

    test: 로직 변경에 따른 테스트 수정

--- src/test/java/com/moabam/api/application/payment/PaymentServiceTest.java
+++ src/test/java/com/moabam/api/application/payment/PaymentServiceTest.java
@@ -9,6 +9,7 @@ import static org.mockito.BDDMockito.*;
 import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -18,12 +19,15 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import com.moabam.api.application.bug.BugService;
 import com.moabam.api.application.coupon.CouponService;
 import com.moabam.api.domain.payment.Payment;
+import com.moabam.api.domain.payment.PaymentStatus;
 import com.moabam.api.domain.payment.repository.PaymentRepository;
 import com.moabam.api.domain.payment.repository.PaymentSearchRepository;
 import com.moabam.api.dto.payment.ConfirmPaymentRequest;
 import com.moabam.api.dto.payment.ConfirmTossPaymentResponse;
 import com.moabam.api.dto.payment.PaymentRequest;
+import com.moabam.api.infrastructure.payment.TossPaymentService;
 import com.moabam.global.error.exception.NotFoundException;
+import com.moabam.global.error.exception.TossPaymentException;
 
 @ExtendWith(MockitoExtension.class)
 class PaymentServiceTest {
@@ -36,6 +40,9 @@ class PaymentServiceTest {
 
 	@Mock
 	CouponService couponService;
+
+	@Mock
+	TossPaymentService tossPaymentService;
 
 	@Mock
 	PaymentRepository paymentRepository;
@@ -58,35 +65,59 @@ class PaymentServiceTest {
 			.hasMessage("존재하지 않는 결제 정보입니다.");
 	}
 
-	@DisplayName("결제 정보 검증 시 해당 결제 정보가 존재하지 않으면 예외가 발생한다.")
-	@Test
-	void validate_info_not_found_exception() {
-		// given
-		Long memberId = 1L;
-		Payment payment = payment(bugProduct());
-		ConfirmPaymentRequest request = confirmPaymentRequest();
-		given(paymentSearchRepository.findByOrderId(request.orderId())).willReturn(Optional.empty());
+	@DisplayName("결제 승인을 처리한다.")
+	@Nested
+	class ProcessConfirm {
 
-		// when, then
-		assertThatThrownBy(() -> paymentService.validateInfo(memberId, request))
-			.isInstanceOf(NotFoundException.class)
-			.hasMessage("존재하지 않는 결제 정보입니다.");
-	}
+		@DisplayName("해당 결제 정보가 존재하지 않으면 예외가 발생한다.")
+		@Test
+		void validate_info_not_found_exception() {
+			// given
+			Long memberId = 1L;
+			ConfirmPaymentRequest request = confirmPaymentRequest();
+			given(paymentSearchRepository.findByOrderId(request.orderId())).willReturn(Optional.empty());
 
-	@DisplayName("결제 승인 시 쿠폰을 적용한 경우 쿠폰을 차감한 후 벌레를 충전한다.")
-	@Test
-	void confirm_with_coupon_success() {
-		// given
-		Long memberId = 1L;
-		Long couponWalletId = 1L;
-		Payment payment = paymentWithCoupon(bugProduct(), discount1000Coupon(), couponWalletId);
-		ConfirmTossPaymentResponse response = confirmTossPaymentResponse();
+			// when, then
+			assertThatThrownBy(() -> paymentService.processConfirm(memberId, request))
+				.isInstanceOf(NotFoundException.class)
+				.hasMessage("존재하지 않는 결제 정보입니다.");
+		}
 
-		// when
-		paymentService.confirm(memberId, payment, response);
+		@DisplayName("토스 결제 승인 요청이 실패하면 결제 실패 처리한다.")
+		@Test
+		void toss_fail() {
+			// given
+			Long memberId = 1L;
+			Payment payment = payment(bugProduct());
+			ConfirmPaymentRequest request = confirmPaymentRequest();
+			given(paymentSearchRepository.findByOrderId(request.orderId())).willReturn(Optional.of(payment));
+			given(tossPaymentService.confirm(request)).willThrow(TossPaymentException.class);
 
-		// then
-		verify(couponService, times(1)).discount(couponWalletId, memberId);
-		verify(bugService, times(1)).charge(memberId, payment.getProduct());
+			// when, then
+			assertThatThrownBy(() -> paymentService.processConfirm(memberId, request))
+				.isInstanceOf(TossPaymentException.class);
+			assertThat(payment.getPaymentKey()).isEqualTo(PAYMENT_KEY);
+			assertThat(payment.getStatus()).isEqualTo(PaymentStatus.ABORTED);
+		}
+
+		@DisplayName("쿠폰을 적용한 경우 쿠폰을 차감한 후 벌레를 충전한다.")
+		@Test
+		void success_with_coupon() {
+			// given
+			Long memberId = 1L;
+			Long couponWalletId = 1L;
+			Payment payment = paymentWithCoupon(bugProduct(), discount1000Coupon(), couponWalletId);
+			ConfirmPaymentRequest request = confirmPaymentRequest();
+			ConfirmTossPaymentResponse response = confirmTossPaymentResponse();
+			given(paymentSearchRepository.findByOrderId(request.orderId())).willReturn(Optional.of(payment));
+			given(tossPaymentService.confirm(request)).willReturn(response);
+
+			// when
+			paymentService.processConfirm(memberId, request);
+
+			// then
+			verify(couponService, times(1)).discount(couponWalletId, memberId);
+			verify(bugService, times(1)).charge(memberId, payment.getProduct());
+		}
 	}
 }
