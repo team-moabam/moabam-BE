fd99886a886af183f697d06ce0bfb0ee664182d6
parksey <parkseyeon99@naver.com>
2023-12-01 21:57:52 +0900

    style: 변수면 변경

--- src/main/java/com/moabam/admin/domain/admin/AdminRepository.java
+++ src/main/java/com/moabam/admin/domain/admin/AdminRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AdminRepository extends JpaRepository<Admin, Long> {
 
-	Optional<Admin> findBySocialId(String s);
+	Optional<Admin> findBySocialId(String socialId);
 }
