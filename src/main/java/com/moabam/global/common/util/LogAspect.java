a4a14ff2712ac4c9c0ff5c8b7296a23c2f70aff3
Youngmyung Kim <83266154+ymkim97@users.noreply.github.com>
2023-12-03 20:46:00 +0900

    feat: exception AOP 로그 추가 (#241)
    * feat: ExceptionHandler AOP 적용
    
    * refactor: 수정
    
    * refactor: checkstyle 적용

---
+++ src/main/java/com/moabam/global/common/util/LogAspect.java
@@ -0,0 +1,24 @@
+package com.moabam.global.common.util;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Aspect
+@Slf4j
+@Component
+public class LogAspect {
+
+	@Around("execution(* com.moabam.global.error.handler.GlobalExceptionHandler.*(..))")
+	public Object printExceptionLog(ProceedingJoinPoint joinPoint) throws Throwable {
+		Object[] args = joinPoint.getArgs();
+		Exception exception = (Exception)args[0];
+
+		log.error("===== EXCEPTION LOG =====", exception);
+
+		return joinPoint.proceed();
+	}
+}
