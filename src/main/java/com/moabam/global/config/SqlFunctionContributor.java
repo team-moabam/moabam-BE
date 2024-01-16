cee698ad72955dbb129b0b19e6bd4d124c50b47d
ymkim97 <rladudaud97@naver.com>
2024-01-16 16:48:56 +0900

    refactor: Match Against 변경

--- src/main/java/com/moabam/global/config/SqlFunctionContributor.java
+++ src/main/java/com/moabam/global/config/SqlFunctionContributor.java
@@ -11,7 +11,12 @@ public class SqlFunctionContributor implements FunctionContributor {
 	public void contributeFunctions(FunctionContributions functionContributions) {
 		functionContributions.getFunctionRegistry()
 			.registerPattern(
-				"match_against", "MATCH (?1) AGAINST (?2 IN BOOLEAN MODE)",
+				"match_against", "MATCH (?1) AGAINST (?2 IN NATURAL LANGUAGE MODE)",
+				functionContributions.getTypeConfiguration().getBasicTypeRegistry().resolve(DOUBLE));
+
+		functionContributions.getFunctionRegistry()
+			.registerPattern(
+				"match_against_two", "MATCH (?1, ?2) AGAINST (?3 IN NATURAL LANGUAGE MODE)",
 				functionContributions.getTypeConfiguration().getBasicTypeRegistry().resolve(DOUBLE));
 	}
 }
