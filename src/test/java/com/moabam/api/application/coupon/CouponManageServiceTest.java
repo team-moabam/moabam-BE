b233ca80b385b1cf475db76e6e34d6473d3b724d
홍혁준 <hongdosan@naver.com>
2024-07-03 20:34:02 +0900

    hotfix

--- src/test/java/com/moabam/api/application/coupon/CouponManageServiceTest.java
+++ src/test/java/com/moabam/api/application/coupon/CouponManageServiceTest.java
@@ -8,6 +8,7 @@ import java.time.LocalDate;
 import java.util.Optional;
 import java.util.Set;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
